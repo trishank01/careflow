2f93285a58978f5772531f76f069f4197ff92582
tk <trishank01@gmail.com>
2026-07-08 23:02:39 +0530

    chore: remove default EKS addons from Terraform management

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -26,15 +26,6 @@ module "eks" {
   enable_irsa = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
     aws-ebs-csi-driver = {
       most_recent              = true
       service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
